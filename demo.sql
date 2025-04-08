8f04350d375baa924709c9e5e017feffa19ca3f9
Abhi Udayshankar <abhi.udayshankar@kellogg.northwestern.edu>
2025-04-08 16:30:24 +0000

    completed week 2 sql labs

--- demo.sql
+++ demo.sql
@@ -1,4 +1,7 @@
 -- Get the first and last name of every player in the database.
 -- Note: this is a code comment in SQL (meaning it's documentation and ignored by the computer).
 
+-- Add a code comment by adding two hyphens
+-- Run this code by typing in .read demo.sql instead of manually
+
 SELECT first_name, last_name FROM players;
